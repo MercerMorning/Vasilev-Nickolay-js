bd7fb8a237952f65333b93b46ce9f38d04119696
MercerMorning <hugopochta@gmail.com>
2020-11-04 22:02:48 +0300

    'начало_вп1'

--- projects/yandex_map/index.js
+++ projects/yandex_map/index.js
@@ -20,6 +20,7 @@
                     (res) => {
                         if (res.geoObjects.getLength()) {
                             // var point = res.geoObjects.get(0);
+                            console.log(res.geoObjects.get(0).properties.getAll())
                             address = res.geoObjects.get(0).properties.get('name');
                             console.log(address)
                             // myMap.panTo(point.geometry.getCoordinates());
@@ -29,7 +30,42 @@
                     alert("Возникла ошибка: " + error.message);
                     }
                 )
-                myMap.balloon.open(coords, {
+
+                // var myBalloonContentLayout = ymaps.templateLayoutFactory.createClass(
+                //     '<div class="myBalloon">' +
+                //     '<h3>$[contentHeader]</h3>' +
+                //     '<p><strong>Адрес:</strong> $[contentBody]</p>' +
+                //     '<button>Добавить</button>' +
+                //     '</div>'
+                // );
+
+                // var myBalloonCloseLayout = ymaps.templateLayoutFactory.createClass(
+                //     '<button>close</button>'
+                // );
+
+                var myBalloonLayout = ymaps.templateLayoutFactory.createClass(
+                    `<div class="balloonContainer">
+                    <div class="balloonHeader"> 
+                    <p class="address">${address}</p><i class="fas fa-times close-btn"></i>
+                    </div>
+                    <div class="balloonContent">
+                    <h3>Ваш отзыв</h3>
+                    <input type="text" placeholder="Ваше имя">
+                    <input type="text" placeholder="Укажите место">
+                    <input type="text" placeholder="Поделитесь впечатлениями">
+                    </div>
+                    <div class="balloonFooter">
+                        <button class="addButton">Добавить</button>
+                    </div>
+                    </div>`
+                );
+     
+                ymaps.layout.storage.add('my#layout', myBalloonLayout);
+                // ymaps.layout.storage.add('my#contentlayout', myBalloonContentLayout);
+                // ymaps.layout.storage.add('my#contentCloselayout', myBalloonCloseLayout);
+                
+                await myMap.balloon.open(coords, {
+                    layout: 'my#contentlayout',
                     contentHeader:address,
                     contentBody: [
                         '<input type="text" placeholder="Укажите ваше имя">',
@@ -39,7 +75,20 @@
                         '<textarea placeholder="Оставьте отзыв"></textarea>'
                     ].join(''),
                     contentFooter:'<button>Добавить</button>'
-                });
+                },
+                { 
+                    layout: 'my#layout',
+                    // contentLayout: 'my#contentlayout',
+                    // closeButtonLayout:  'my#contentCloselayout',
+                },
+                );
+
+                var closeBtn = document.body.querySelector('.close-btn');
+                closeBtn.onclick = function () {
+                    myMap.balloon.close()
+                }
+
+               
             }
             else {
                 myMap.balloon.close();
