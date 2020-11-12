ec12c8fab3741a186f92a13d1cddb29c18c06b44
MercerMorning <hugopochta@gmail.com>
2020-11-12 14:53:06 +0300

    'процесс_импорта_модулей'

--- projects/yandex_map/index.js
+++ projects/yandex_map/index.js
@@ -1,196 +1,146 @@
-    ymaps.ready(init);
-     
-    window.addEventListener('storage', function(e) {  
-        console.log(e.key);
-      });
 
-    const storage = localStorage;
+import { dateWrite } from './helper';
+import ymaps from 'ymaps';
 
-    // storage.clear()
+ymaps.load('https://api-maps.yandex.ru/2.1/?apikey=4040c59a-ba5a-403b-8e3a-7b3a6ac9f8a9&lang=ru_RU').then(maps => {
+    maps.ready(init(maps));
+});
 
-    var myMap,
-        prevBtn,
-        addBtn,
-        closeBtn,
-        inputName,
-        inputPlace,
-        inputDesc,
-        coords,
-        clusterer,
-        markers;
-
-    let placemarks = [];
-
-    async function openPlacemarkBalloon(address) {
-        clusterer.balloon.close();
-
-        let bal = clusterer.getGeoObjects().filter(obj => obj.properties.get("address") == address)
-        let comments = bal[0].properties.get('comments');
-        
-        await myMap.balloon.open(coords, {
-            contentHeader: address,
-            contentFooter: comments,
-            address: address,
-            comments: comments,
-        },
-        { 
-            layout: 'my#layout',
-            address: address,
-            comments: comments,
-        },
-        );
-        setButtons( bal[0].geometry.getCoordinates());
-
-        
-        // let bal = clusterer.getGeoObjects().filter(obj => obj.properties.get("address") == address)
-        // let error = true;
-        // while(error) {
-        //     try {
-                // myMap.setCenter(bal[0].geometry.getCoordinates());
-        //         bal[0].balloon.open()
-        //         error = false;
-        //     } catch {
-        //         myMap.setZoom( myMap.getZoom() + 1 );
-        //         continue;
-        //     }
-        // }
-        
-        
-        
-        // bal[0].balloon.open();
-    }
-
-    function setButtons(coords) {
-        addBtn = document.body.querySelector('.add-btn')
-        closeBtn = document.body.querySelector('.close-btn')
-        inputName = document.body.querySelector('#inputName ')
-        inputPlace = document.body.querySelector('#inputPlace')
-        inputDesc = document.body.querySelector('#inputDesc')
-        
-        addBtn.onclick = function () {
+    function init(ymaps){
+        window.addEventListener('storage', function(e) {  
+            console.log(e.key);
+          });
+    
+        const storage = localStorage;
+    
+        // storage.clear()
+    
+        var myMap,
+            prevBtn,
+            addBtn,
+            closeBtn,
+            inputName,
+            inputPlace,
+            inputDesc,
+            coords,
+            clusterer,
+            markers;
+    
+        let placemarks = [];
+    
+       
+    
+        function setButtons(coords) {
+            addBtn = document.body.querySelector('.add-btn')
+            closeBtn = document.body.querySelector('.close-btn')
+            inputName = document.body.querySelector('#inputName ')
+            inputPlace = document.body.querySelector('#inputPlace')
+            inputDesc = document.body.querySelector('#inputDesc')
             
-            if (inputName.value && inputPlace.value && inputDesc.value) {
-                let storageContent = storage.data ? JSON.parse(storage.data) : [];
+            addBtn.onclick = function () {
                 
-                storageContent.push({
-                    coords: coords,
-                    commentatorName: inputName.value,
-                    place: inputPlace.value,
-                    desc: inputDesc.value,
-                });
-                
-                console.log(storageContent)
-                storage.data = JSON.stringify(storageContent)
-                
-                closeBtn.click();
-                // newMarker(coords)
-                
-                fillMarkers();
+                if (inputName.value && inputPlace.value && inputDesc.value) {
+                    let storageContent = storage.data ? JSON.parse(storage.data) : [];
+                    
+                    storageContent.push({
+                        coords: coords,
+                        commentatorName: inputName.value,
+                        place: inputPlace.value,
+                        desc: inputDesc.value,
+                    });
+                    
+                    console.log(storageContent)
+                    storage.data = JSON.stringify(storageContent)
+                    
+                    closeBtn.click();
+                    // newMarker(coords)
+                    
+                    fillMarkers();
+                }
+            }
+    
+            closeBtn.onclick = function () {
+                myMap.balloon.close()
             }
         }
-
-        closeBtn.onclick = function () {
-            myMap.balloon.close()
-        }
-    }
-
-    let fillMarkers = () => {
-        let storageList = JSON.parse(storage.data);
-        
-        clusterer.removeAll();
-        placemarks = [];
-        console.log(placemarks)
-        markers = [];
-
-        for (item of storageList) {
-          if (markers.find( (element) => {
-              return element.coords.join('') === item.coords.join('');
-          })) {
-              markers.find( (element) => {
+    
+        let fillMarkers = () => {
+            let storageList = JSON.parse(storage.data);
+            
+            clusterer.removeAll();
+            placemarks = [];
+            console.log(placemarks)
+            markers = [];
+            console.log(storageList)
+    
+            for (let item of storageList) {
+              if (markers.find( (element) => {
                   return element.coords.join('') === item.coords.join('');
-              }).comment.push(
-                  {
+              })) {
+                  markers.find( (element) => {
+                      return element.coords.join('') === item.coords.join('');
+                  }).comment.push(
+                      {
+                          date: dateWrite(),
+                          author: item.commentatorName,
+                          place: item.place,
+                          desc: item.desc,
+                      }
+                  );
+              }
+              else {
+                markers.push({
+                  coords: item.coords,
+                  comment: [{
                       date: dateWrite(),
                       author: item.commentatorName,
                       place: item.place,
                       desc: item.desc,
-                  }
-              );
-          }
-          else {
-            markers.push({
-              coords: item.coords,
-              comment: [{
-                  date: dateWrite(),
-                  author: item.commentatorName,
-                  place: item.place,
-                  desc: item.desc,
-              }]
-            });
-          }
+                  }]
+                });
+              }
+            }
+          
+          
+            for (let marker of markers) {
+              newMarker(marker.coords, marker.comment);
+            }
         }
-      
-      
-        for (let marker of markers) {
-          newMarker(marker.coords, marker.comment);
-        }
-    }
-
-    async function newMarker(coords, comments = []) {
-        var geocoder = new ymaps.geocode(coords, {results: 1});
-        var address;
-        await geocoder.then(
-            (res) => {
-                if (res.geoObjects.getLength()) {
-                    address = res.geoObjects.get(0).properties.get('name');
+    
+        async function newMarker(coords, comments = []) {
+            var geocoder = new ymaps.geocode(coords, {results: 1});
+            var address;
+            await geocoder.then(
+                (res) => {
+                    if (res.geoObjects.getLength()) {
+                        address = res.geoObjects.get(0).properties.get('name');
+                    }
+                },
+                function (error) {
+                    alert("Возникла ошибка: " + error.message);
                 }
-            },
-            function (error) {
-                alert("Возникла ошибка: " + error.message);
-            }
-        )
-        let placemark =  new ymaps.Placemark(coords,
-            {
-                address: address,
-                comments: comments,
-            },
-            {
-                iconLayout: 'default#image',
-                iconImageHref: 'placemark.png',
-                iconImageSize: [23, 28],
-                balloonLayout: 'my#layout',
-            }
-        )
-        placemark.events.add('balloonopen',  function (e) { 
-            coords = placemark.geometry.getCoordinates();
-            setButtons(coords);
-        })
-        // placemark.balloon.open()
-        clusterer.add(placemark)
-        return placemark;
-    }
+            )
+            let placemark =  new ymaps.Placemark(coords,
+                {
+                    address: address,
+                    comments: comments,
+                },
+                {
+                    iconLayout: 'default#image',
+                    // iconImageHref: './placemark.png',
+                    iconImageSize: [23, 28],
+                    balloonLayout: 'my#layout',
+                }
+            )
+            placemark.events.add('balloonopen',  function (e) { 
+                coords = placemark.geometry.getCoordinates();
+                setButtons(coords);
+            })
+            // placemark.balloon.open()
+            clusterer.add(placemark)
+            return placemark;
+        }
 
-    function dateWrite() {
-        var date = new Date();
-    
-        var day = date.getDate();
-    
-        if (day < 10) day = '0' + day;
-    
-        var month = date.getMonth() + 1;
-    
-        if (month < 10) month = '0' + month;
-    
-        var year = date.getFullYear();
-    
-    
-        var d = day + '.' + month + '.' + year;
-       
-        return d;
-    }
-    
-
-    function init(){
         myMap = new ymaps.Map("map", {
             center: [55.76, 37.64],
             zoom: 7,
@@ -291,7 +241,7 @@
                 {% for geoObject in properties.geoObjects %}
                     <div class="balloonContainer baloon-slide">
                         <div class="balloonHeader"> 
-                            <p class="address" onclick="openPlacemarkBalloon( '{{ geoObject.properties.address }}' )">
+                            <p class="address">
                                 {{ geoObject.properties.address }}
                             </p>
                             <i class="fas fa-times close-btn"></i>
@@ -325,45 +275,57 @@
         ].join(''));
 
         function setSlideButtons() {
-            let nextBtn = document.body.querySelector('.next-slide'),
-                prevBtn = document.body.querySelector('.previous-slide'),
-                balloonClaster = document.body.querySelector('.balloonClaster');
-                currentSlide =  balloonClaster.firstElementChild,
-                actionsContainer = document.body.querySelector('.actions');
+            let addressLinks = document.body.querySelectorAll('.clusterer-address');
+            let clustererBaloon = document.body.querySelector('.clusterer-baloon');
+            console.log(clustererBaloon)
+            clustererBaloon.addEventListener("DOMSubtreeModified", function() {
+                alert(123)
+              });
+            document.getElementById("demo1").addEventListener("DOMSubtreeModified", function() {
+                document.getElementById("demo2").innerHTML = "demo1 Changed!"
+              });
+            for (let addressLink of addressLinks) {
+                console.log(addressLink)
+                addressLink.addEventListener('click', function() {
+                    openPlacemarkBalloon(addressLink.dataset.address)
+                })
+            }
 
                 // console.log(balloonClaster.firstElementChild)
-            currentSlide.classList.add('active')
+            // currentSlide.classList.add('active')
                 
-            nextBtn = document.body.querySelector('.next-slide')
+            // nextBtn = document.body.querySelector('.next-slide')
             
-            actionsContainer.addEventListener('click', (e) => {
-                if (e.target.className == 'next-slide' && currentSlide.nextElementSibling && currentSlide.nextElementSibling.classList.contains('baloon-slide')) {
-                    console.log(currentSlide.nextElementSibling)
-                    currentSlide.classList.remove('active');
-                    currentSlide.nextElementSibling.classList.add('active')
-                    currentSlide = currentSlide.nextElementSibling;
-                } else {
-                    currentSlide.classList.remove('active');
-                    currentSlide = balloonClaster.firstElementChild;
-                    currentSlide.nextElementSibling.classList.add('active')
-                }
-                if (e.target.className == 'previous-slide' && currentSlide.previousElementSibling && currentSlide.previousElementSibling.classList.contains('baloon-slide')) {
-                    currentSlide.classList.remove('active');
-                    currentSlide.previousElementSibling.classList.add('active')
-                    currentSlide = currentSlide.previousElementSibling;
-                } else {
-                    // currentSlide.classList.remove('active');
-                    // currentSlide = balloonClaster.lastElementChild;
-                    // currentSlide.previousElementSibling.classList.add('active')
-                }
-            })
+            // actionsContainer.addEventListener('click', (e) => {
+            //     if (e.target.className == 'next-slide' && currentSlide.nextElementSibling && currentSlide.nextElementSibling.classList.contains('baloon-slide')) {
+            //         console.log(currentSlide.nextElementSibling)
+            //         currentSlide.classList.remove('active');
+            //         currentSlide.nextElementSibling.classList.add('active')
+            //         currentSlide = currentSlide.nextElementSibling;
+            //     } else {
+            //         currentSlide.classList.remove('active');
+            //         currentSlide = balloonClaster.firstElementChild;
+            //         currentSlide.nextElementSibling.classList.add('active')
+            //     }
+            //     if (e.target.className == 'previous-slide' && currentSlide.previousElementSibling && currentSlide.previousElementSibling.classList.contains('baloon-slide')) {
+            //         currentSlide.classList.remove('active');
+            //         currentSlide.previousElementSibling.classList.add('active')
+            //         currentSlide = currentSlide.previousElementSibling;
+            //     } else {
+            //         // currentSlide.classList.remove('active');
+            //         // currentSlide = balloonClaster.lastElementChild;
+            //         // currentSlide.previousElementSibling.classList.add('active')
+            //     }
+            // })
         }
 
         var customItemContentLayout = ymaps.templateLayoutFactory.createClass(
             // The "raw" flag means that data is inserted "as is" without escaping HTML.
-            ` <p class="address" onclick="openPlacemarkBalloon( '{{ geoObject.properties.address }}' )">
+            ` 
+            <div class="clusterer-baloon">
+            <a href='#' class="clusterer-address" data-address="{{ geoObject.properties.address }}">
             {{ properties.address|raw }}
-            </p>
+            </a>
             {% if contentFooter %}
                 {% for comment in contentFooter %}
                     <b>{{ comment.author }}</b>
@@ -404,6 +366,45 @@
         myMap.geoObjects.add(clusterer)
 
         fillMarkers()
+
+        async function openPlacemarkBalloon(address) {
+            clusterer.balloon.close();
+    
+            let bal = clusterer.getGeoObjects().filter(obj => obj.properties.get("address") == address)
+            let comments = bal[0].properties.get('comments');
+            
+            await myMap.balloon.open(coords, {
+                contentHeader: address,
+                contentFooter: comments,
+                address: address,
+                comments: comments,
+            },
+            { 
+                layout: 'my#layout',
+                address: address,
+                comments: comments,
+            },
+            );
+            setButtons( bal[0].geometry.getCoordinates());
+    
+            
+            // let bal = clusterer.getGeoObjects().filter(obj => obj.properties.get("address") == address)
+            // let error = true;
+            // while(error) {
+            //     try {
+                    // myMap.setCenter(bal[0].geometry.getCoordinates());
+            //         bal[0].balloon.open()
+            //         error = false;
+            //     } catch {
+            //         myMap.setZoom( myMap.getZoom() + 1 );
+            //         continue;
+            //     }
+            // }
+            
+            
+            
+            // bal[0].balloon.open();
+        }
     }
 
-    
+    import './index.html'
